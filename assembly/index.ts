eea1dc217fbd2a5ee3ded27378c6ffc047a690a8
Patrick Dubroy <pdubroy@gmail.com>
2023-10-04 12:23:35 +0200

    Implementation of optimize with call_indirect

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,14 @@
-// The entry file of your WebAssembly module.
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+export function optimize(numParams: u32, iterations: u32): void {
+  const epsilon: f64 = 0.0001
+  let i = iterations
+  while (i > 0) {
+    call_indirect<f64>(0) // Evaluate loss
+    for (let i: u32 = 0; i < numParams; ++i) {
+      const diff = call_indirect<f64>(i + 1) // Evaluate gradient
+      const old = load<f64>(i * 8)
+      const update = old - diff * epsilon
+      store<f64>(i * 8, update)
+    }
+    i = i - 1
+  }
 }
