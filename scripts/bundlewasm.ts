08f6ba8ca98813178f5835fad688b8c1764b3b21
Patrick Dubroy <pdubroy@gmail.com>
2023-10-10 12:46:52 +0200

    Extract typesec, funcsec, *and* codesec from prebuilt module

--- scripts/bundlewasm.ts
+++ scripts/bundlewasm.ts
@@ -1,5 +1,5 @@
 import fs from "node:fs"
-import { extractCodesec } from "./extractCodesec.ts"
+import { extractFunctions } from "./extractFunctions.ts"
 
 /*
   Extracts the code section from the AssemblyScript release build
@@ -8,20 +8,27 @@ import { extractCodesec } from "./extractCodesec.ts"
 
 const inputPath = "../build/release.wasm"
 const inputUrl = new URL(inputPath, import.meta.url)
-const outputUrl = new URL(inputPath + "_codesec.ts", import.meta.url)
+const outputUrl = new URL(inputPath + "_sections.ts", import.meta.url)
 
 const buf = fs.readFileSync(inputUrl)
-const { entryCount, contents } = extractCodesec(buf)
+const sections = extractFunctions(buf)
 
-const base64Contents = JSON.stringify(Buffer.from(contents).toString("base64"))
-const output = `const bytes = atob(${base64Contents});
-const buf = new Uint8Array(bytes.length);
-for (let i = 0; i < bytes.length; i++) {
-  buf[i] = bytes.charCodeAt(i);
-}
-export default {
-  entryCount: ${JSON.stringify(entryCount)},
-  contents: buf
-}
+let output = ""
+for (const [secName, { entryCount, contents }] of Object.entries(sections)) {
+  const base64Contents = JSON.stringify(
+    Buffer.from(contents).toString("base64"),
+  )
+  output += `export const ${secName} = (() => {
+  const bytes = atob(${base64Contents});
+  const buf = new Uint8Array(bytes.length);
+  for (let i = 0; i < bytes.length; i++) {
+    buf[i] = bytes.charCodeAt(i);
+  }
+  return {
+    entryCount: ${JSON.stringify(entryCount)},
+    contents: Array.from(buf)
+  }
+})()
 `
+}
 fs.writeFileSync(outputUrl, output, "utf8")
