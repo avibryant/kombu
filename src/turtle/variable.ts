ed54249ab3ae34a3f34bf599b1b8159f08397659
Avi Bryant <avi@avibryant.com>
2023-10-15 16:20:24 -0700

    split up interface

--- src/turtle/variable.ts
+++ src/turtle/variable.ts
@@ -1,26 +1,18 @@
 import * as t from '../core/types'
-import * as c from '../core/construct'
 import {Evaluator} from '../core/eval'
 
-export abstract class Variable {
-    name: string
-    units: string
+export interface VariableDisplay {
     param: t.Param
-    value: t.Num
+    loss: number
+    value: number
+    min: number
+    max: number
+    units: string
+    update: (y: number) => number 
+}
 
-    constructor(name: string, units: string) {
-        this.name = name
-        this.units = units
-        this.param = c.param(name)
-        this.value = this.transform(this.param)
-    }
-
-    abstract transform(param: t.Param): t.Num
-    abstract logProb(y: t.Num): t.Num
-
-    abstract display(ev: Evaluator): number
-    abstract inverse(y: number): number
-
-    abstract min(): number
-    abstract max(): number
+export interface Variable {
+    param: t.Param
+    loss: t.Num
+    display: (ev: Evaluator) => VariableDisplay
 }
