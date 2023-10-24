96b43e146a5e0909f875adf469c0721798a813cc
Avi Bryant <avi@avibryant.com>
2023-10-24 20:39:07 +0100

    address warnings

--- src/turtle/draw.ts
+++ src/turtle/draw.ts
@@ -4,12 +4,12 @@ import { degrees } from "./vec2"
 export function draw(t: Turtle) {
   const side = t.approxLength("A", 100)
 
-  const o = t.position
+ // const o = t.position
   t.forward(side)
   t.right(degrees(90))
   t.forward(side)
 
-  const d = t.position
+ // const d = t.position
 
   t.atMouse()
   t.right(degrees(90))
