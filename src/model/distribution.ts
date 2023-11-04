c202d293d2996d6590d1c4b17b3e4cf7e7cec01d
Avi Bryant <avi@avibryant.com>
2023-11-03 21:00:02 -0700

    remove var, change angle rep

--- src/model/distribution.ts
+++ src/model/distribution.ts
@@ -2,7 +2,7 @@ import * as k from "../core/api"
 
 export type Distribution = MeanSD
 export interface MeanSD {
-  type: "normal" | "lognormal"
+  type: "normal" | "lognormal" | "laplace"
   mean: k.Num
   sd: k.Num
 }
@@ -23,18 +23,33 @@ export function logNormal(mean: k.AnyNum, sd: k.AnyNum): Distribution {
   }
 }
 
+export function laplace(mean: k.AnyNum, sd: k.AnyNum): Distribution {
+  return {
+    type: "laplace",
+    mean: k.num(mean),
+    sd: k.num(sd),
+  }
+}
+
 export function logP(dist: Distribution, value: k.Num): k.Num {
   switch (dist.type) {
     case "normal":
       return normalLogP(dist, value)
     case "lognormal":
       return logNormalLogP(dist, value)
+    case "laplace":
+      return laplaceLogP(dist, value)
   }
 }
 
 function normalLogP(dist: MeanSD, value: k.Num): k.Num {
   const diff = k.sub(value, dist.mean)
   return k.div(k.mul(diff, diff), k.mul(2, k.mul(dist.sd, dist.sd)))
+}
+
+function laplaceLogP(dist: MeanSD, value: k.Num): k.Num {
+  const diff = k.sub(value, dist.mean)
+  return k.sub(k.div(k.abs(diff), dist.sd), k.log(k.mul(2, dist.sd)))
 }
 
 function logNormalLogP(dist: MeanSD, value: k.Num): k.Num {
