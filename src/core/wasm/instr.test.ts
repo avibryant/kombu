c82b9fc46646e0ffb10e4f488219e2503d9923f9
Patrick Dubroy <pdubroy@gmail.com>
2023-11-20 09:02:21 +0100

    Low-level IR with pretty printing (#29)
    - Introduce a low-level representation that corresponds to Wasm instructions.
    - Nodes can be associated with an `Expr` in the high-level IR or an arbitrary string.
    - `prettyPrint` prints instructions in a format very similar to the WebAssembly text format (.wat), with source info (the `Expr` or string) included as comments
    - `toBytes` converts to a `number[]` representing the raw bytecode stream
    
    Example:
    
    ```
    const load = i.f64_load("brilliant load", 99)
    const store = i.f64_store("amazing store", 99, i.f64_const("y", 123.456))
    const seq = i.seq("brrrr", load, store)
    expect(i.prettyPrint(seq)).toEqual(dedent`
      (; brrrr ;)
      (; brilliant load ;)
      i32.const 99
      f64.load {3,0}
      (; amazing store ;)
      i32.const 99
      f64.const 123.456
      f64.store {3,0}
    `)
    ```
    
    TODOs for future PRs:
    - [ ] Update handling of `KOMBU_DEBUG` so that you can use that var to dump instructions
    - [ ] Improve formatting of comments to make it easy to align with high-level IR. Maybe show nesting depth?

---
+++ src/core/wasm/instr.test.ts
@@ -0,0 +1,95 @@
+import * as w from "@wasmgroundup/emit"
+import { dedent } from "ts-dedent"
+import { expect, test } from "vitest"
+
+import * as prebuilt from "../../../build/release.wasm_sections"
+import { builtins } from "./builtins"
+import * as i from "./instr"
+
+const powIdx =
+  builtins.findIndex((fn) => fn.name === "pow") + prebuilt.importsec.entryCount
+
+test("const", () => {
+  expect(i.prettyPrint(i.f64_const(null, 99))).toEqual("f64.const 99")
+  expect(i.prettyPrint(i.f64_const(null, 12345.678))).toEqual(
+    "f64.const 12345.678",
+  )
+
+  expect(i.toBytes(i.f64_const(null, 99))).toEqual([
+    0x44, 0, 0, 0, 0, 0, 192, 88, 64,
+  ])
+})
+
+test("binary", () => {
+  const l = i.f64_const(null, 123.456)
+  const r = i.f64_const(null, 99)
+  const bin = i.f64_binOp(null, "add", l, r)
+
+  expect(i.prettyPrint(bin)).toEqual(dedent`
+    f64.const 123.456
+    f64.const 99
+    f64.add
+  `)
+
+  expect(i.toBytes(bin)).toEqual(
+    [
+      [w.instr.f64.const, w.f64(123.456)],
+      [w.instr.f64.const, w.f64(99)],
+      w.instr.f64.add,
+    ].flat(2),
+  )
+})
+
+test("load", () => {
+  const load = i.f64_load(null, 99)
+  expect(i.prettyPrint(load)).toEqual(dedent`
+    i32.const 99
+    f64.load {3,0}
+  `)
+  expect(i.toBytes(load)).toEqual(
+    [
+      [w.instr.i32.const, w.i32(99)],
+      [w.instr.f64.load, [3, 0]],
+    ].flat(2),
+  )
+})
+
+test("store", () => {
+  const store = i.f64_store(null, 99, i.f64_const("y", 123.456))
+  expect(i.prettyPrint(store)).toEqual(dedent`
+    i32.const 99
+    f64.const 123.456
+    f64.store {3,0}
+  `)
+  expect(i.toBytes(store)).toEqual(
+    [
+      [w.instr.i32.const, w.i32(99)],
+      [w.instr.f64.const, w.f64(123.456)],
+      [w.instr.f64.store, [3, 0]],
+    ].flat(2),
+  )
+})
+
+test("call", () => {
+  const call = i.callBuiltin(null, "pow")
+  expect(i.prettyPrint(call)).toEqual(dedent`
+    call $${powIdx} (pow)
+  `)
+  expect(i.toBytes(call)).toEqual([w.instr.call, ...w.funcidx(powIdx)])
+})
+
+test("prettyPrint - seq with source", () => {
+  const load = i.f64_load("load!", 99)
+  const store = i.f64_store("store!", 99, i.f64_const("y", 123.456))
+  const seq = i.seq("brrrr", load, store)
+  expect(i.prettyPrint(seq)).toEqual(dedent`
+    (; brrrr ;)
+    (; load! ;)
+    i32.const 99
+    f64.load {3,0}
+    (; store! ;)
+    i32.const 99
+    f64.const 123.456
+    f64.store {3,0}
+  `)
+})
--- src/core/wasm/instr.ts
+++ src/core/wasm/instr.ts
@@ -2,31 +2,219 @@ import * as w from "@wasmgroundup/emit"
 
 import * as prebuilt from "../../../build/release.wasm_sections"
 import { builtins } from "./builtins"
+import { Expr as IrExpr, pseudocode } from "../ir"
+
+type FragmentSource = IrExpr | string | null
+
+export enum WasmType {
+  Binary,
+  Const,
+  Load,
+  Store,
+  Call,
+  Seq,
+}
+
+export type WasmInstr =
+  | BinaryInstr
+  | ConstInstr
+  | LoadInstr
+  | StoreInstr
+  | CallInstr
+export type WasmFragment = WasmInstr | WasmInstrSeq
+
+export interface WasmInstrSeq {
+  type: WasmType.Seq
+  source: FragmentSource
+  instrs: WasmFragment[]
+}
+
+export interface BinaryInstr {
+  type: WasmType.Binary
+  source: FragmentSource
+  valtype: "i32" | "f64"
+  op: "mul" | "add"
+
+  // These must be WasmFragment, and not WasmInstr, in order to handle calls
+  // that take arguments.
+  l: WasmFragment
+  r: WasmFragment
+}
+
+export interface ConstInstr {
+  type: WasmType.Const
+  source: FragmentSource
+  valtype: "i32" | "f64"
+  value: number
+}
+
+export interface LoadInstr {
+  type: WasmType.Load
+  source: FragmentSource
+  valtype: "f64"
+  addr: WasmInstr
+}
+
+export interface StoreInstr {
+  type: WasmType.Store
+  source: FragmentSource
+  valtype: "f64"
+  addr: WasmInstr
+  // This must be WasmFragment, and not WasmInstr, in order to handle calls
+  // that take arguments.
+  expr: WasmFragment
+}
+
+export interface CallInstr {
+  type: WasmType.Call
+  source: FragmentSource
+  funcidx: number
+  name: string
+}
 
 // Required as an immediate arg for all loads/stores.
-const ALIGNMENT_AND_OFFSET = w.memarg(3 /* bits */, 0)
+const ALIGNMENT_AND_OFFSET: number[] = w.memarg(3 /* bits */, 0).flat(1)
 
-export function callBuiltin(name: string): w.BytecodeFragment {
-  const idx = builtins.findIndex((fn) => fn.name === name)
+export function callBuiltin(source: FragmentSource, name: string): CallInstr {
+  const idx =
+    builtins.findIndex((fn) => fn.name === name) + prebuilt.importsec.entryCount
   if (idx === -1) throw new Error(`builtin '${name}' not found`)
-  return [w.instr.call, w.funcidx(prebuilt.importsec.entryCount + idx)]
+  return {
+    type: WasmType.Call,
+    source,
+    funcidx: idx,
+    name,
+  }
 }
 
-export function f64_const(v: number) {
-  return [w.instr.f64.const, w.f64(v)]
+export function i32_const(source: FragmentSource, v: number): ConstInstr {
+  return { type: WasmType.Const, source, valtype: "i32", value: v }
 }
 
-export function f64_load(offset: number) {
-  return [
-    [w.instr.i32.const, w.i32(offset)],
-    [w.instr.f64.load, ALIGNMENT_AND_OFFSET],
-  ]
+export function f64_const(source: FragmentSource, v: number): ConstInstr {
+  return { type: WasmType.Const, source, valtype: "f64", value: v }
 }
 
-export function f64_store(offset: number, frag: w.BytecodeFragment) {
-  return [
-    [w.instr.i32.const, w.i32(offset)],
-    frag,
-    [w.instr.f64.store, ALIGNMENT_AND_OFFSET],
-  ]
+export function f64_binOp(
+  source: FragmentSource,
+  op: BinaryInstr["op"],
+  l: WasmFragment,
+  r: WasmFragment,
+): BinaryInstr {
+  return {
+    type: WasmType.Binary,
+    source,
+    valtype: "f64",
+    op,
+    l,
+    r,
+  }
+}
+
+export function f64_load(source: FragmentSource, offset: number): LoadInstr {
+  return {
+    type: WasmType.Load,
+    source,
+    valtype: "f64",
+    addr: i32_const(null, offset),
+  }
+}
+
+export function f64_store(
+  source: FragmentSource,
+  offset: number,
+  expr: WasmFragment,
+): StoreInstr {
+  return {
+    type: WasmType.Store,
+    source,
+    valtype: "f64",
+    addr: i32_const("store addr", offset),
+    expr,
+  }
+}
+
+export function seq(
+  source: FragmentSource,
+  ...instrs: WasmFragment[]
+): WasmInstrSeq {
+  return {
+    type: WasmType.Seq,
+    source,
+    instrs,
+  }
+}
+
+function prettySource(source: FragmentSource) {
+  if (source == null) return ""
+  const str = typeof source === "string" ? source : pseudocode(source)
+  return `(; ${str} ;)`
+}
+
+export function prettyPrint(frag: WasmFragment): string {
+  const srcInfo = prettySource(frag.source)
+
+  // Join lines, discard the first one if it's empty.
+  const joinLines = (info: string, ...rest: string[]) =>
+    (info === "" ? rest : [info, ...rest]).join("\n")
+
+  switch (frag.type) {
+    case WasmType.Const:
+      // Ignore srcInfo, as it's not useful for const.
+      return `${frag.valtype}.const ${frag.value}`
+    case WasmType.Binary:
+      return joinLines(
+        srcInfo,
+        prettyPrint(frag.l),
+        prettyPrint(frag.r),
+        `${frag.valtype}.${frag.op}`,
+      )
+    case WasmType.Load:
+      return joinLines(
+        srcInfo,
+        prettyPrint(frag.addr),
+        `${frag.valtype}.load {${ALIGNMENT_AND_OFFSET}}`,
+      )
+    case WasmType.Store:
+      return joinLines(
+        srcInfo,
+        prettyPrint(frag.addr),
+        prettyPrint(frag.expr),
+        `${frag.valtype}.store {${ALIGNMENT_AND_OFFSET}}`,
+      )
+    case WasmType.Call:
+      return `call $${frag.funcidx} (${frag.name})`
+    case WasmType.Seq:
+      return joinLines(srcInfo, ...frag.instrs.flatMap(prettyPrint))
+  }
+}
+
+export function toBytes(frag: WasmFragment): number[] {
+  switch (frag.type) {
+    case WasmType.Const:
+      return frag.valtype === "f64"
+        ? [w.instr.f64.const, ...w.f64(frag.value)]
+        : [w.instr.i32.const, ...w.i32(frag.value)]
+    case WasmType.Binary:
+      return [
+        toBytes(frag.l),
+        toBytes(frag.r),
+        w.instr[frag.valtype][frag.op],
+      ].flat(3)
+    case WasmType.Load:
+      return [
+        toBytes(frag.addr),
+        [w.instr.f64.load, ALIGNMENT_AND_OFFSET],
+      ].flat(3)
+    case WasmType.Store:
+      return [
+        toBytes(frag.addr),
+        toBytes(frag.expr),
+        [w.instr.f64.store, ALIGNMENT_AND_OFFSET],
+      ].flat(3)
+    case WasmType.Call:
+      return [w.instr.call, ...w.funcidx(frag.funcidx)]
+    case WasmType.Seq:
+      return frag.instrs.flatMap(toBytes)
+  }
 }
