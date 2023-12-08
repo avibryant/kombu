a9aa6199f4a93954946aa1fcfbad278ecf1cb39b
Patrick Dubroy <pdubroy@gmail.com>
2023-12-08 11:34:32 +0100

    cleanup

--- src/core/jsopt.ts
+++ src/core/jsopt.ts
@@ -6,36 +6,35 @@ import { optimizeLBFGS, optimizeGradientDescent } from "../../build/asopt"
 import { initEvaluatorState, getParamEntries } from "./asinterop"
 
 export function jsOptimizer(loss: Loss, params: Map<t.Param, number>) {
-  return {
-    optimize(
-      maxIterations: number,
-      observations: Map<t.Param, number>,
-      opts?: OptimizeOptions,
-    ): Map<t.Param, number> {
-      const options = {
-        ...defaultOptions,
-        ...(opts ?? {}),
-      }
+  function optimize(
+    maxIterations: number,
+    observations: Map<t.Param, number>,
+    opts?: OptimizeOptions,
+  ): Map<t.Param, number> {
+    const options = {
+      ...defaultOptions,
+      ...(opts ?? {}),
+    }
 
-      initEvaluatorState(loss, params, observations)
-      switch (options.method) {
-        case "LBFGS":
-          optimizeLBFGS(
-            loss.freeParams.length,
-            maxIterations,
-            options.m,
-            options.epsilon,
-          )
-          break
-        case "GradientDescent":
-          optimizeGradientDescent(
-            loss.freeParams.length,
-            maxIterations,
-            options.learningRate,
-          )
-          break
-      }
-      return new Map(getParamEntries())
-    },
+    initEvaluatorState(loss, params, observations)
+    switch (options.method) {
+      case "LBFGS":
+        optimizeLBFGS(
+          loss.freeParams.length,
+          maxIterations,
+          options.m,
+          options.epsilon,
+        )
+        break
+      case "GradientDescent":
+        optimizeGradientDescent(
+          loss.freeParams.length,
+          maxIterations,
+          options.learningRate,
+        )
+        break
+    }
+    return new Map(getParamEntries())
   }
+  return { optimize }
 }
