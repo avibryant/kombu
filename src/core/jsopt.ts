aa9e5d01330fa5c2bee13370276345067233a257
Patrick Dubroy <pdubroy@gmail.com>
2023-12-11 14:51:26 +0100

    cleanup

--- src/core/jsopt.ts
+++ src/core/jsopt.ts
@@ -1,12 +1,14 @@
 import { checkNotNull } from "./assert"
 import { evaluator } from "./eval"
-import * as lbfgs from './lbfgs'
+import * as lbfgs from "./lbfgs"
 import { Loss } from "./loss"
 import * as t from "./types"
 import { OptimizeOptions, defaultOptions } from "./wasmopt"
 
-import { optimizeGradientDescent } from "../../build/asopt"
+import { optimizeGradientDescent, optimizeLBFGS } from "../../build/asopt"
 import { getParamEntries, initEvaluatorState } from "./asinterop"
+
+const usePenroseLBFGS = true
 
 export function jsOptimizer(loss: Loss, params: Map<t.Param, number>) {
   function optimize(
@@ -19,15 +21,20 @@ export function jsOptimizer(loss: Loss, params: Map<t.Param, number>) {
       ...(opts ?? {}),
     }
 
+    if (options.method === "LBFGS" && usePenroseLBFGS) {
+      return new Map(optimizeLBFGS2(loss, params, observations, maxIterations))
+    }
+
     initEvaluatorState(loss, params, observations)
     switch (options.method) {
-        case "LBFGS":
-        const newEntries = optimizeNewLBFGS(
-          loss,
-          params,
-          observations,
-          10)
-        return new Map(newEntries)
+      case "LBFGS":
+        optimizeLBFGS(
+          loss.freeParams.length,
+          maxIterations,
+          options.m,
+          options.epsilon,
+        )
+        break
       case "GradientDescent":
         optimizeGradientDescent(
           loss.freeParams.length,
@@ -41,30 +48,20 @@ export function jsOptimizer(loss: Loss, params: Map<t.Param, number>) {
   return { optimize }
 }
 
-export function optimizeNewLBFGS(
+export function optimizeLBFGS2(
   loss: Loss,
   freeParams: Map<t.Param, number>,
   observations: Map<t.Param, number>,
   maxIterations: number,
-  // m: uint,
-  // eps: double,
+  // m: number,
+  // eps: number,
 ): [t.Param, number][] {
   const paramEntries = Array.from(freeParams.entries())
-
-  const evalLossAndGrad = (x: Float64Array, gradout: Float64Array) => {
-    const ev = evaluator(new Map([
-      ...paramEntries.map(([p, _], i): [t.Param, number] => [p, x[i]]),
-      ...observations.entries(),
-    ]))
-    const ret = ev.evaluate(loss.value)
-    paramEntries.forEach(([p, _], i) => {
-      gradout[i] = ev.evaluate(checkNotNull(loss.gradient.elements.get(p)))
-    })
-    return ret
-  }
+  const grad = paramEntries.map(([p]) =>
+    checkNotNull(loss.gradient.elements.get(p)),
+  )
 
   const xs = new Float64Array(paramEntries.map(([_, val]) => val))
-  // let fxs = 0
   const gradfxs = new Float64Array(freeParams.size)
   const gradientPreconditioned = new Float64Array(gradfxs)
   let normGradfxs = 0
@@ -76,38 +73,43 @@ export function optimizeNewLBFGS(
     maxSteps: 10,
     epsd: 1e-11,
   }
+  const evalLossAndGrad = (x: Float64Array, gradout: Float64Array) => {
+    const ev = evaluator(
+      new Map([
+        ...paramEntries.map(([p, _], i): [t.Param, number] => [p, x[i]]),
+        ...observations.entries(),
+      ]),
+    )
+    const ret = ev.evaluate(loss.value)
+    grad.forEach((num, i) => {
+      gradout[i] = ev.evaluate(num)
+    })
+    return ret
+  }
   const state = lbfgs.firstStep(cfg, evalLossAndGrad, xs)
 
-  let it = 0
+  let stepCount = 0
+  lbfgs.stepUntil(cfg, evalLossAndGrad, xs, state, (info) => {
+    if (stepCount++ > maxIterations) return false
 
-  lbfgs.stepUntil(
-    cfg,
-    evalLossAndGrad,
-    xs,
-    state,
-    (info) => {
-      if (it++ > maxIterations) return false
+    if (containsNaN(info.state.x)) {
+      console.log("xs", xs)
+      throw Error("NaN in xs")
+    }
+    gradfxs.set(info.state.grad)
+    if (containsNaN(gradfxs)) {
+      console.log("gradfxs", gradfxs)
+      throw Error("NaN in gradfxs")
+    }
+    gradientPreconditioned.set(info.r)
 
-      if (containsNaN(info.state.x)) {
-        console.log("xs", xs)
-        throw Error("NaN in xs")
-      }
-      // fxs = info.fx
-      gradfxs.set(info.state.grad)
-      if (containsNaN(gradfxs)) {
-        console.log("gradfxs", gradfxs)
-        throw Error("NaN in gradfxs")
-      }
-      gradientPreconditioned.set(info.r)
-
-      // Don't take the Euclidean norm. According to Boyd (485), we should use the Newton descent check, with the norm of the gradient pulled back to the nicer space.
-      normGradfxs = dot(gradfxs, gradientPreconditioned)
-      if (normGradfxs < uoStop) {
-        return false
-      }
-      return undefined
-    },
-  )
+    // Don't take the Euclidean norm. According to Boyd (485), we should use the Newton descent check, with the norm of the gradient pulled back to the nicer space.
+    normGradfxs = dot(gradfxs, gradientPreconditioned)
+    if (normGradfxs < uoStop) {
+      return false
+    }
+    return undefined
+  })
   return paramEntries.map(([p, _], i) => [p, xs[i]])
 }
 
@@ -119,4 +121,4 @@ function dot(xs: Float64Array, ys: Float64Array) {
   return z
 }
 
-const containsNaN = (arr: Float64Array) => arr.some(n => Number.isNaN(n))
+const containsNaN = (arr: Float64Array) => arr.some((n) => Number.isNaN(n))
