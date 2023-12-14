5f79564a7474f817a429fad33a76d788146a4f53
Patrick Dubroy <pdubroy@gmail.com>
2023-12-14 16:26:47 +0100

    Revert import order changes

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,32 +1,32 @@
 export type { Evaluator } from "./eval"
-export type { Gradient } from "./grad"
-export type { Loss } from "./loss"
 export type { AnyNum } from "./num"
 export type { Optimizer } from "./optimize"
 export type { OptimizeOptions } from "./options"
 export type { Num, Param } from "./types"
+export type { Loss } from "./loss"
+export type { Gradient } from "./grad"
 
-export { nodeCount, observation, one, param, zero } from "./construct"
+export { param, observation, zero, one, nodeCount } from "./construct"
+export { tex } from "./tex"
 export { evaluator } from "./eval"
 export { gradient } from "./grad"
+export { optimize, optimizer } from "./optimize"
 export { loss } from "./loss"
 export {
-  abs,
+  num,
   add,
-  atan,
-  cos,
+  sub,
+  mul,
   div,
+  pow,
+  sqrt,
+  neg,
+  abs,
+  sign,
+  cos,
+  sin,
+  atan,
   exp,
   log,
-  mul,
-  neg,
-  num,
-  pow,
-  sign,
-  sin,
-  sqrt,
-  sub,
 } from "./num"
-export { optimize, optimizer } from "./optimize"
 export { logistic, softplus } from "./stats"
-export { tex } from "./tex"
